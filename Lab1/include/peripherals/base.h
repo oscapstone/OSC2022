c9ebc8b57a5c192f6006e27d1dc6bca6a6dcd3a4
LittleLaGi <yam5421363@gmail.com>
2022-02-23 14:39:28 +0800

    [Lab1] use pointer instead of get32/put32

--- Lab1/include/peripherals/base.h
+++ Lab1/include/peripherals/base.h
@@ -4,6 +4,6 @@
 // There is a VideoCore/ARM MMU translating physical addresses to bus addresses.
 // The MMU maps physical address 0x3f000000 to bus address 0x7e000000.
 
-#define PBASE 0x3F000000
+#define MMIO_BASE 0x3F000000
 
 #endif  /*_P_BASE_H */
