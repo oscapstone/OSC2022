4261ff10805539a4866304804fe709fd1a4e5f7f
Pttsai <50416832+b0938930511@users.noreply.github.com>
2022-03-01 13:32:45 +0800

    lab1

---
+++ lab1/include/StringUtils.h
@@ -0,0 +1,8 @@
+#ifndef __STRING_UTILS_H
+#define __STRING_UTILS_H
+
+void stripString(char *str);
+int compareString(const char *s1, const char *s2);
+void utils_uint2str_hex(unsigned int num, char* str) ;
+
+#endif
