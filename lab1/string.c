a1832c7756caa382e12365090bb3f3f2aec5e673
Patrick Star <jason89041209@gmail.com>
2022-03-09 20:13:15 +0800

    :pencil: Update strcmp

--- lab1/string.c
+++ lab1/string.c
@@ -1,18 +1,7 @@
 int strcmp(char *s1, char *s2) {
-    int value;
-
-    s1--, s2--;
-    do {
-        s1++, s2++;
-        if (*s1 == *s2) {
-            value = 0;
-        } else if (*s1 < *s2) {
-            value = -1;
-            break;
-        } else {
-            value = 1;
-            break;
-        }
-    } while (*s1 != 0 && *s2 != 0);
-    return value;
+    while (*s1 != '\0' && *s1 == *s2) {
+        s1++;
+        s2++;
+    }
+    return (*(unsigned char *)s1) - (*(unsigned char *)s2);
 }
