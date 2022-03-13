ad3b30fd11232ccd2876f56496e6dbde018923a3
b4435242 <49785533+b4435242@users.noreply.github.com>
2022-03-13 19:06:35 +0800

    Delete src directory

--- src/lib.h
+++
@@ -1,2 +0,0 @@
-int _strncmp(const char *s1, const char *s2, register int n);
-void* _memset(void *b, int c, int len);
--- src/reset.h
+++
@@ -1,3 +0,0 @@
-void reset(int tick);
-void cancel_reset();
-void set(long addr, unsigned int value);
