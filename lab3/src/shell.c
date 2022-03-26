7e6372c26c212cc50e06730eca47094fefa034de
a13579and2468 <a13579and2468@a13579and2468-X555LD>
2022-03-26 17:27:46 +0800

    fix asyncIO bug with critical section

--- lab3/src/shell.c
+++ lab3/src/shell.c
@@ -11,19 +11,20 @@ void shell()
 {
     char cmd[MAX_BUF_SIZE];
     print_system_messages();
-    core_timer_enable();
     timer_list_init();
-    while(1)
+    core_timer_enable();
+    while (1)
     {
         uart_async_printf("# ");
         uart_async_gets(cmd);
         do_cmd(cmd);
     }
+    uart_printf("QQ");
 }
 
-void do_cmd(char* cmd)
+void do_cmd(char *cmd)
 {
-    if(strcmp(cmd,"help")==0)
+    if (strcmp(cmd, "help") == 0)
     {
         uart_async_printf("help                            : print this help menu\r\n");
         uart_async_printf("hello                           : print Hello World!\r\n");
@@ -35,48 +36,51 @@ void do_cmd(char* cmd)
         uart_async_printf("setTimeout [MESSAGE] [SECONDS]  : print message after [SECONDS] seconds (non-blocking)\r\n");
         uart_async_printf("clockAlert                      : alert every two seconds\r\n");
     }
-    else if(strcmp(cmd,"hello")==0)
+    else if (strcmp(cmd, "hello") == 0)
     {
         uart_async_printf("Hello World!\r\n");
     }
-    else if(strcmp(cmd,"reboot")==0)
+    else if (strcmp(cmd, "reboot") == 0)
     {
         reboot();
-    }else if(strcmp(cmd,"cat")==0)
+    }
+    else if (strcmp(cmd, "cat") == 0)
     {
         uart_async_printf("Filename: ");
         char filepath[MAX_BUF_SIZE];
         uart_async_gets(filepath);
         cat(filepath);
-
-    }else if(strcmp(cmd,"ls")==0)
+    }
+    else if (strcmp(cmd, "ls") == 0)
     {
         ls(".");
-
-    }else if(strcmp(cmd,"show_device_tree")==0)
+    }
+    else if (strcmp(cmd, "show_device_tree") == 0)
     {
-        traverse_device_tree(dtb_place,dtb_callback_show_tree);
-
-    }else if(strcmp(cmd,"exec")==0) // in filesystem.c
+        traverse_device_tree(dtb_place, dtb_callback_show_tree);
+    }
+    else if (strcmp(cmd, "exec") == 0) // in filesystem.c
     {
         uart_async_printf("Filename: ");
         char filepath[MAX_BUF_SIZE];
         uart_async_gets(filepath);
         execfile(filepath);
-
-    }else if(strncmp(cmd,"setTimeout",sizeof("setTimeout")-1)==0)
+    }
+    else if (strncmp(cmd, "setTimeout", sizeof("setTimeout") - 1) == 0)
     {
-        char* message = strchr(cmd,' ') + 1;
-        char* end_message = strchr(message,' ');
+        char *message = strchr(cmd, ' ') + 1;
+        char *end_message = strchr(message, ' ');
         *end_message = '\0';
-        char* seconds = end_message+1;
-        add_timer(uart_puts,atoi(seconds),message);
-
-    }else if(strcmp(cmd,"clockAlert")==0)
+        char *seconds = end_message + 1;
+        add_timer(uart_puts, atoi(seconds), message);
+    }
+    else if (strcmp(cmd, "clockAlert") == 0)
     {
-        add_timer(two_second_alert,2,"two_second_alert");
-    }else{
-        uart_async_printf("Unknown command!\r\n");
+        add_timer(two_second_alert, 2, "two_second_alert");
+    }
+    else
+    {
+        uart_async_printf("Unknown command!: %s\r\n", cmd);
     }
 }
 
@@ -84,12 +88,12 @@ void print_system_messages()
 {
     unsigned int board_revision;
     get_board_revision(&board_revision);
-    uart_async_printf("Board revision is : 0x%x\r\n",board_revision);
-    
+    uart_async_printf("Board revision is : 0x%x\r\n", board_revision);
+
     unsigned int arm_mem_base_addr;
     unsigned int arm_mem_size;
 
-    get_arm_memory_info(&arm_mem_base_addr,&arm_mem_size);
-    uart_async_printf("ARM memory base address in bytes : 0x%x\r\n",arm_mem_base_addr);
-    uart_async_printf("ARM memory size in bytes : 0x%x\r\n",arm_mem_size);
+    get_arm_memory_info(&arm_mem_base_addr, &arm_mem_size);
+    uart_async_printf("ARM memory base address in bytes : 0x%x\r\n", arm_mem_base_addr);
+    uart_async_printf("ARM memory size in bytes : 0x%x\r\n", arm_mem_size);
 }
--- lab3/src/timer.c
+++ lab3/src/timer.c
@@ -4,133 +4,142 @@
 #include "malloc.h"
 #include "string.h"
 
-struct list_head* timer_event_list;  // first head has nothing, store timer_event_t after it 
+struct list_head *timer_event_list; // first head has nothing, store timer_event_t after it
 
-void timer_list_init(){
+void timer_list_init()
+{
     INIT_LIST_HEAD(timer_event_list);
 }
 
-void core_timer_enable(){
+void core_timer_enable()
+{
     __asm__ __volatile__(
         "mov x1, 1\n\t"
         "msr cntp_ctl_el0, x1\n\t" // enable
 
         "mrs x1, cntfrq_el0\n\t"
         "mov x2, 0x100000\n\t"
-        "mul x1, x1, x2\n\t"    //set a big value prevent interrupt immediately
+        "mul x1, x1, x2\n\t"        //set a big value prevent interrupt immediately
         "msr cntp_tval_el0, x1\n\t" // set expired time
 
         "mov x2, 2\n\t"
         "ldr x1, =" XSTR(CORE0_TIMER_IRQ_CTRL) "\n\t"
-        "str w2, [x1]\n\t" // unmask timer interrupt
+                                               "str w2, [x1]\n\t" // unmask timer interrupt
     );
 }
 
-void timer_event_callback(timer_event_t * timer_event){
+void timer_event_callback(timer_event_t *timer_event)
+{
 
-    list_del_entry((struct list_head*)timer_event); // delete the event
-    free(timer_event->args);  // free the arg space
+    list_del_entry((struct list_head *)timer_event); // delete the event
+    free(timer_event->args);                         // free the arg space
     free(timer_event);
-    ((void (*)(char*))timer_event-> callback)(timer_event->args);  // call the callback store in event
+    ((void (*)(char *))timer_event->callback)(timer_event->args); // call the callback store in event
 
     //set interrupt to next time_event if existing
-    if(!list_empty(timer_event_list))
+    if (!list_empty(timer_event_list))
     {
-        set_core_timer_interrupt_by_tick(((timer_event_t*)timer_event_list->next)->interrupt_time);
-    }else
-    {
-        set_core_timer_interrupt(10000);  // disable timer interrupt (set a very big value)
+        set_core_timer_interrupt_by_tick(((timer_event_t *)timer_event_list->next)->interrupt_time);
     }
-    
+    else
+    {
+        set_core_timer_interrupt(10000); // disable timer interrupt (set a very big value)
+    }
 }
 
-void two_second_alert(char* str)
+void two_second_alert(char *str)
 {
     unsigned int cntpct_el0;
-    __asm__ __volatile__("mrs %0, cntpct_el0\n\t": "=r"(cntpct_el0)); //tick now
+    __asm__ __volatile__("mrs %0, cntpct_el0\n\t"
+                         : "=r"(cntpct_el0)); //tick now
 
     unsigned int cntfrq_el0;
-    __asm__ __volatile__("mrs %0, cntfrq_el0\n\t": "=r"(cntfrq_el0)); //tick frequency
+    __asm__ __volatile__("mrs %0, cntfrq_el0\n\t"
+                         : "=r"(cntfrq_el0)); //tick frequency
 
-    uart_printf("alert '%s': \r\nexception irq_router ->  seconds after booting : %d\r\n",str,cntpct_el0/cntfrq_el0);
+    uart_printf("alert '%s': \r\nexception irq_router ->  seconds after booting : %d\r\n", str, cntpct_el0 / cntfrq_el0);
 
-    add_timer(two_second_alert,2,"two_second_alert");
+    add_timer(two_second_alert, 2, "two_second_alert");
 }
 
-void core_timer_handler(){
-
+void core_timer_handler()
+{
     timer_event_callback((timer_event_t *)timer_event_list->next); // do callback and set new interrupt
-
 }
 
 // give a string argument to callback   timeout after seconds
-void add_timer(void *callback, unsigned long long timeout, char* args){
+void add_timer(void *callback, unsigned long long timeout, char *args)
+{
 
-    timer_event_t* the_timer_event = kmalloc(sizeof(timer_event_t)); //need to free by event handler
+    timer_event_t *the_timer_event = kmalloc(sizeof(timer_event_t)); //need to free by event handler
 
     // store argument string into timer_event
-    the_timer_event->args = kmalloc(strlen(args)+1);
-    strcpy(the_timer_event -> args,args);
-    
-    the_timer_event->interrupt_time = get_tick_plus_s(timeout);  // store interrupt time into timer_event
+    the_timer_event->args = kmalloc(strlen(args) + 1);
+    strcpy(the_timer_event->args, args);
+
+    the_timer_event->interrupt_time = get_tick_plus_s(timeout); // store interrupt time into timer_event
     the_timer_event->callback = callback;
     INIT_LIST_HEAD(&the_timer_event->listhead);
 
     // add the timer_event into timer_event_list (sorted)
-    struct list_head* curr;
+    struct list_head *curr;
 
-    list_for_each(curr,timer_event_list)
+    list_for_each(curr, timer_event_list)
     {
-        if(((timer_event_t*)curr)->interrupt_time > the_timer_event->interrupt_time)
+        if (((timer_event_t *)curr)->interrupt_time > the_timer_event->interrupt_time)
         {
-            list_add(&the_timer_event->listhead,curr->prev);  // add this timer at the place just before the bigger one (sorted)
+            list_add(&the_timer_event->listhead, curr->prev); // add this timer at the place just before the bigger one (sorted)
             break;
         }
     }
 
-    
-    if(list_is_head(curr,timer_event_list))
+    if (list_is_head(curr, timer_event_list))
     {
-        list_add_tail(&the_timer_event->listhead,timer_event_list);  // for the time is the biggest
+        list_add_tail(&the_timer_event->listhead, timer_event_list); // for the time is the biggest
     }
 
     // set interrupt to first event
-    set_core_timer_interrupt_by_tick(((timer_event_t*)timer_event_list->next)->interrupt_time); 
+    set_core_timer_interrupt_by_tick(((timer_event_t *)timer_event_list->next)->interrupt_time);
 }
 
 // get cpu tick add some second
-unsigned long long get_tick_plus_s(unsigned long long second){
+unsigned long long get_tick_plus_s(unsigned long long second)
+{
 
-    unsigned long long cntpct_el0=0;
-    __asm__ __volatile__("mrs %0, cntpct_el0\n\t": "=r"(cntpct_el0)); //tick now
+    unsigned long long cntpct_el0 = 0;
+    __asm__ __volatile__("mrs %0, cntpct_el0\n\t"
+                         : "=r"(cntpct_el0)); //tick now
 
-    unsigned long long cntfrq_el0=0;
-    __asm__ __volatile__("mrs %0, cntfrq_el0\n\t": "=r"(cntfrq_el0)); //tick frequency
+    unsigned long long cntfrq_el0 = 0;
+    __asm__ __volatile__("mrs %0, cntfrq_el0\n\t"
+                         : "=r"(cntfrq_el0)); //tick frequency
 
-    return (cntpct_el0 + cntfrq_el0*second);
+    return (cntpct_el0 + cntfrq_el0 * second);
 }
 
-
 // set timer interrupt time to [expired_time] seconds after now (relatively)
-void set_core_timer_interrupt(unsigned long long expired_time){
+void set_core_timer_interrupt(unsigned long long expired_time)
+{
     __asm__ __volatile__(
-        "mrs x1, cntfrq_el0\n\t"   //cntfrq_el0 -> relative time
+        "mrs x1, cntfrq_el0\n\t" //cntfrq_el0 -> relative time
         "mul x1, x1, %0\n\t"
         "msr cntp_tval_el0, x1\n\t" // set expired time
-    :"=r" (expired_time));
+        : "=r"(expired_time));
 }
 
 // directly set timer interrupt time to a cpu tick  (directly)
-void set_core_timer_interrupt_by_tick(unsigned long long tick){
+void set_core_timer_interrupt_by_tick(unsigned long long tick)
+{
     __asm__ __volatile__(
-        "msr cntp_cval_el0, %0\n\t"  //cntp_cval_el0 -> absolute time
-    :"=r" (tick));
+        "msr cntp_cval_el0, %0\n\t" //cntp_cval_el0 -> absolute time
+        : "=r"(tick));
 }
 
-int timer_list_get_size(){
+int timer_list_get_size()
+{
     int r = 0;
-    struct list_head* curr;
-    list_for_each(curr,timer_event_list)
+    struct list_head *curr;
+    list_for_each(curr, timer_event_list)
     {
         r++;
     }
