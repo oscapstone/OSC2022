a60c905ff0fca4ece7a83e856efbdf4285d6ec9b
JusticeLeee <franklp97531@gmail.com>
2022-03-27 13:50:21 +0800

    clean code, sync lab2 to lab3

---
+++ lab3/run.sh
@@ -0,0 +1,4 @@
+sudo chmod 777 /dev/ttyUSB1
+# python3 ./script/send_file.py -s bootloader.img
+python3 ./script/send_file.py -s kernel8.img -t /dev/ttyUSB1
+# python3 ./script/send_file.py -s kernel8.img -t /dev/pts/9
