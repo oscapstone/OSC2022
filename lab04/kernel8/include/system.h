3b75bc6b077362e831c015a091f178078bc5a639
howeiisou <howeiisou@csie.io>
2022-04-14 18:28:52 +0800

    lab04

---
+++ lab04/kernel8/include/system.h
@@ -0,0 +1,23 @@
+#ifndef __SYSTEM_H__
+#define __SYSTEM_H__
+#include "utils.h"
+#include "uart.h"
+#include "uart_boot.h"
+#include "cpio.h"
+#include "dtb_parser.h"
+#include "irq.h"
+#include "timer.h"
+#include "mm.h"
+
+#define SYS_CMD_NUM 16
+
+void system_command(char*);
+
+void help();
+void hello();
+void reset(int tick);
+void cancel_reset();
+
+
+
+#endif
