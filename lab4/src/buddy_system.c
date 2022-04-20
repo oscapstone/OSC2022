6dc1ddc2986b111fb43928a5dc746a224936727c
Patrick Star <jason89041209@gmail.com>
2022-04-21 01:24:31 +0800

    :pencil: Update Advanced 2, 3

--- lab4/src/buddy_system.c
+++ lab4/src/buddy_system.c
@@ -1,11 +1,11 @@
 #include "list.h"
 #include "uart.h"
 #include "utils.h"
-#define MEMORY_BASE 0x10000000
+#define MEMORY_BASE 0x0
 #define PAGESIZE 0x1000 // 4KB
-#define MAX_PAGES 64
-#define LOG2_MAX_PAGES 6
-#define LOG2_MAX_PAGES_PLUS_1 7
+#define MAX_PAGES 0x40000
+#define LOG2_MAX_PAGES 18
+#define LOG2_MAX_PAGES_PLUS_1 19
 #define BELONG_LEFT -1
 #define ALLOCATED -2
 
@@ -13,10 +13,67 @@ struct buddy_system_node {
     int index;
     int level;
     struct list_head list;
-} node[MAX_PAGES];
+};
 
-struct list_head head[LOG2_MAX_PAGES_PLUS_1];
-int frame_array[MAX_PAGES];
+struct buddy_system_node *node;
+struct list_head *head;
+int *frame_array;
+
+extern char _end;
+
+void buddy_system_update_list(int index) {
+    int level = frame_array[index];
+    if (level == LOG2_MAX_PAGES || level == BELONG_LEFT || level == ALLOCATED)
+        return;
+
+    int buddy = index ^ pow2(level);
+    if (frame_array[buddy] != level)
+        return;
+
+    int frist = index < buddy ? index : buddy;
+    int second = index < buddy ? buddy : index;
+
+    list_del_init(&node[frist].list);
+    list_del_init(&node[second].list);
+    frame_array[frist] = level + 1;
+    frame_array[second] = BELONG_LEFT;
+    node[frist].level = level + 1;
+    list_add(&node[frist].list, &head[level + 1]);
+    buddy_system_update_list(frist);
+
+    return;
+}
+
+void *simple_malloc(void **cur, unsigned long size) {
+    void *ret = *cur;
+    *cur = *(char **)cur + size;
+    return ret;
+}
+
+void reserve_memory(unsigned long start, unsigned long end) {
+    for (int i = 0; i < MAX_PAGES; i++) {
+        if (frame_array[i] != 0 && frame_array[i] != ALLOCATED && frame_array[i] != BELONG_LEFT) {
+            uart_puts("i = ");
+            uart_hex(i);
+            uart_puts("\n");
+            uart_puts("frame_array[i] = ");
+            uart_hex(frame_array[i]);
+            uart_puts("\n");
+        }
+    }
+    int start_page = (start - MEMORY_BASE) / PAGESIZE;
+    int end_page = (end - MEMORY_BASE - 1) / PAGESIZE;
+    uart_puts("start_page: ");
+    uart_hex(start_page);
+    uart_puts("\n");
+    uart_puts("end_page: ");
+    uart_hex(end_page);
+    uart_puts("\n");
+    for (int i = start_page; i <= end_page; i++) {
+        frame_array[i] = ALLOCATED;
+        list_del_init(&node[i].list);
+    }
+}
 
 void buddy_system_print_all() {
     struct list_head *tmp;
@@ -31,56 +88,49 @@ void buddy_system_print_all() {
         }
         uart_puts("\n");
     }
-    uart_puts("----------- array ----------");
-    for (int i = 0; i < MAX_PAGES; i++) {
-        if (i % 8 == 0)
-            uart_puts("\n");
-        if (frame_array[i] == BELONG_LEFT)
-            uart_puts("--------");
-        else if (frame_array[i] == ALLOCATED)
-            uart_puts("XXXXXXXX");
-        else
-            uart_hex(frame_array[i]);
-        uart_puts(" ");
-    }
+    // uart_puts("----------- array ----------");
+    // for (int i = 0; i < MAX_PAGES; i++) {
+    //     if (i % 8 == 0)
+    //         uart_puts("\n");
+    //     if (frame_array[i] == BELONG_LEFT)
+    //         uart_puts("--------");
+    //     else if (frame_array[i] == ALLOCATED)
+    //         uart_puts("XXXXXXXX");
+    //     else
+    //         uart_hex(frame_array[i]);
+    //     uart_puts(" ");
+    // }
     uart_puts("\n----------------------------\n");
 
     return;
 }
 
 void buddy_system_init() {
+    void *base = (void *)&_end;
+    head = (struct list_head *)simple_malloc(&base, (unsigned long)sizeof(struct list_head) * LOG2_MAX_PAGES_PLUS_1);
+    node = (struct buddy_system_node *)simple_malloc(&base, (unsigned long)sizeof(struct buddy_system_node) * MAX_PAGES);
+    frame_array = (int *)simple_malloc(&base, (unsigned long)sizeof(int) * MAX_PAGES);
+
     for (int i = 0; i < LOG2_MAX_PAGES_PLUS_1; i++)
         INIT_LIST_HEAD(&head[i]);
     for (int i = 0; i < MAX_PAGES; i++) {
         node[i].index = i;
         INIT_LIST_HEAD(&node[i].list);
-        if (i == 0)
-            frame_array[i] = LOG2_MAX_PAGES;
-        else
-            frame_array[i] = BELONG_LEFT;
+        node[i].level = 0;
+        frame_array[i] = 0;
+        list_add_tail(&node[i].list, &head[0]);
     }
-    node[0].level = LOG2_MAX_PAGES;
-    list_add(&node[0].list, &head[LOG2_MAX_PAGES]);
+
+    reserve_memory(0x0, 0x1000);
+    reserve_memory(0x7E000, 0x80000);
+    reserve_memory(0x80000, (unsigned long)base);
+    reserve_memory(0x3c000000, 0x40000000);
+
+    for (int i = 0; i < MAX_PAGES; i++)
+        buddy_system_update_list(i);
 
     buddy_system_print_all();
     return;
-}
-
-void buddy_system_insert_list(struct list_head *node, struct list_head *head) {
-    struct list_head *tmp;
-    if (list_empty(head))
-        list_add(node, head);
-    else {
-        int chk = 0;
-        list_for_each(tmp, head) {
-            if (list_entry(node, struct buddy_system_node, list)->index < list_entry(tmp, struct buddy_system_node, list)->index) {
-                list_add_tail(node, tmp);
-                chk = 1;
-            }
-        }
-        if (!chk)
-            list_add_tail(node, head);
-    }
 }
 
 int buddy_system_find_suitable_size(int size) {
@@ -113,42 +163,6 @@ int buddy_system_find_node_index(int level) {
         return -1;
 
     return list_first_entry(&head[level], struct buddy_system_node, list)->index;
-}
-
-void buddy_system_update_list(int index) {
-    int level = frame_array[index];
-    if (level == LOG2_MAX_PAGES)
-        return;
-
-    struct list_head *prev = node[index].list.prev;
-    struct list_head *next = node[index].list.next;
-    if (prev != &head[level]) {
-        int prev_index = list_entry(prev, struct buddy_system_node, list)->index;
-        if (prev_index / pow2(level + 1) == index / pow2(level + 1)) {
-            list_del_init(&node[index].list);
-            list_del_init(prev);
-            frame_array[index] = BELONG_LEFT;
-            frame_array[prev_index] = level + 1;
-            node[prev_index].level = level + 1;
-            list_add(prev, &head[level + 1]);
-            buddy_system_update_list(prev_index);
-        }
-        return;
-    }
-    if (next != &head[level]) {
-        int next_index = list_entry(next, struct buddy_system_node, list)->index;
-        if (next_index / pow2(level + 1) == index / pow2(level + 1)) {
-            list_del_init(&node[index].list);
-            list_del_init(next);
-            frame_array[next_index] = BELONG_LEFT;
-            frame_array[index] = level + 1;
-            node[index].level = level + 1;
-            list_add(&node[index].list, &head[level + 1]);
-            buddy_system_update_list(index);
-        }
-        return;
-    }
-    return;
 }
 
 unsigned long int buddy_system_alloc(int size) {
@@ -189,7 +203,7 @@ void buddy_system_free(int index) {
     uart_hex(level);
     uart_puts("\n");
 
-    buddy_system_insert_list(&node[index].list, &head[level]);
+    list_add(&node[index].list, &head[level]);
     buddy_system_update_list(index);
     buddy_system_print_all();
 }
