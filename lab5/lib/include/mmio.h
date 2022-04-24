c63286604696c141b0a895e26d94812290eb4692
Charlie Chan <b10702124@gapps.ntust.edu.tw>
2022-04-24 18:35:17 +0800

    feature/init lab5

---
+++ lab5/lib/include/mmio.h
@@ -0,0 +1,6 @@
+
+/* tags */
+#define MAILBOX_TAG_BOARD_VISION     0x10002
+#define MAILBOX_TAG_MEMORY    0x10005
+
+int mailbox_property(unsigned int identifier, unsigned int buffer_size, unsigned char ch);
