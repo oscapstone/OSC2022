97a0618439f6a867eef979ecb1280bd2e62c630b
LittleLaGi <yam5421363@gmail.com>
2022-05-10 00:21:25 +0800

    [Lab6] identity paging for kernel

--- Lab6/include/peripherals/base.h
+++ Lab6/include/peripherals/base.h
@@ -4,6 +4,7 @@
 // There is a VideoCore/ARM MMU translating physical addresses to bus addresses.
 // The MMU maps physical address 0x3f000000 to bus address 0x7e000000.
 
-#define MMIO_BASE 0x3F000000
+#define KVA 0xffff000000000000
+#define MMIO_BASE (KVA + 0x3F000000)
 
 #endif  /*_P_BASE_H */
---
+++ Lab6/include/vm.h
@@ -0,0 +1,25 @@
+#ifndef __VM_H
+#define __VM_H
+
+//number of the most significant bits that must be either all 0s or all 1s
+#define TCR_CONFIG_REGION_48bit (((64 - 48) << 0) | ((64 - 48) << 16))
+//smallest block of memory that can be independently mapped in the translation tables
+#define TCR_CONFIG_4KB ((0b00 << 14) |  (0b10 << 30))
+#define TCR_CONFIG_DEFAULT (TCR_CONFIG_REGION_48bit | TCR_CONFIG_4KB)
+
+#define MAIR_DEVICE_nGnRnE 0b00000000   //peripheral access
+#define MAIR_NORMAL_NOCACHE 0b01000100  //normal RAM access
+#define MAIR_IDX_DEVICE_nGnRnE 0
+#define MAIR_IDX_NORMAL_NOCACHE 1
+#define MAIR_CONFIG_DEFAULT ((MAIR_DEVICE_nGnRnE<<(MAIR_IDX_DEVICE_nGnRnE*8))|(MAIR_NORMAL_NOCACHE<<(MAIR_IDX_NORMAL_NOCACHE*8)))
+
+#define PD_TABLE 0b11
+#define BOOT_PGD_ATTR PD_TABLE
+#define PD_BLOCK 0b01
+#define PD_ACCESS (1 << 10)  //access flag, a page fault is generated if not set
+#define BOOT_PUD_ATTR (PD_ACCESS | (MAIR_IDX_DEVICE_nGnRnE << 2) | PD_BLOCK)  //for L1 (ARM peripherals)
+#define BOOT_L2D_ATTR (PD_ACCESS | (MAIR_IDX_DEVICE_nGnRnE << 2) | PD_BLOCK)  //for L2 (peripherals)
+#define BOOT_L2N_ATTR (PD_ACCESS | (MAIR_IDX_NORMAL_NOCACHE << 2) | PD_BLOCK) //for L2 (normal)
+
+
+#endif
