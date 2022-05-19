d4b3a2a3603fbe59fd2430957eb25118251ebfc3
xiaobye <skps96g313@gmail.com>
2022-05-19 10:03:03 +0800

    [lab3]: finish basic2

--- lab3/kernel/irq_handler.c
+++ lab3/kernel/irq_handler.c
@@ -9,7 +9,10 @@ void irq_handelr(){
     uint32_t core0_irq_source = IO_MMIO_read32(CORE0_INTERRUPT_SOURCE);
     if(core0_irq_source & 2){
         core_timer_irq_handler();
-    }else{
+    }else if(irq_pending_1 & (1 << 29)){
+        LOG("UART");
+    }
+    else{
         LOG("Unkown interrupt, DAIF: %x", get_DAIF());
     }
 }
