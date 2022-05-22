5d9b85a00c81077a04bc895d9fc0701fcbc4ff65
a13579and2468 <a13579and2468@a13579and2468-X555LD>
2022-05-22 16:22:00 +0800

    lab6 basic complete

---
+++ lab6/src/sched.c
@@ -0,0 +1,161 @@
+#include "sched.h"
+#include "exception.h"
+#include "malloc.h"
+#include "timer.h"
+#include "uart.h"
+#include "signal.h"
+#include "mmu.h"
+#include "string.h"
+
+void init_thread_sched()
+{
+    lock();
+    run_queue = kmalloc(sizeof(list_head_t));
+    wait_queue = kmalloc(sizeof(list_head_t));
+    INIT_LIST_HEAD(run_queue);
+    INIT_LIST_HEAD(wait_queue);
+
+    //init pids
+    for (int i = 0; i <= PIDMAX; i++)
+    {
+        threads[i].isused = 0;
+        threads[i].pid = i;
+        threads[i].iszombie = 0;
+    }
+
+    asm volatile("msr tpidr_el1, %0" ::"r"(kmalloc(sizeof(thread_t)))); /// malloc a space for current kernel thread to prevent crash
+
+    thread_t* idlethread = thread_create(idle,0x1000);
+    curr_thread = idlethread;
+    unlock();
+}
+
+void idle(){
+    while(1)
+    {
+        kill_zombies();   //reclaim threads marked as DEAD
+        schedule();       //switch to next thread in run queue
+    }
+}
+
+void schedule(){
+    lock();
+    do{
+        curr_thread = (thread_t *)curr_thread->listhead.next;
+    } while (list_is_head(&curr_thread->listhead, run_queue) || curr_thread->iszombie);
+
+    unlock();
+    switch_to(get_current(), &curr_thread->context);
+}
+
+void kill_zombies(){
+    lock();
+    list_head_t *curr;
+    list_for_each(curr,run_queue)
+    {
+        if (((thread_t *)curr)->iszombie)
+        {
+            list_del_entry(curr);
+            kfree(((thread_t *)curr)->stack_alloced_ptr);        // free stack
+            kfree(((thread_t *)curr)->kernel_stack_alloced_ptr); // free stack
+            free_page_tables(((thread_t *)curr)->context.ttbr0_el1,0);
+            kfree(((thread_t *)curr)->data); // free data (don't free data because of fork)
+            ((thread_t *)curr)->iszombie = 0;
+            ((thread_t *)curr)->isused = 0;
+        }
+    }
+    unlock();
+}
+
+int exec_thread(char *data, unsigned int filesize)
+{
+    thread_t *t = thread_create(data, filesize);
+
+    mappages(t->context.ttbr0_el1, 0x3C000000L, 0x3000000L, 0x3C000000L);
+    mappages(t->context.ttbr0_el1, 0x3F000000L, 0x1000000L, 0x3F000000L);
+    mappages(t->context.ttbr0_el1, 0x40000000L, 0x40000000L, 0x40000000L);
+
+    mappages(t->context.ttbr0_el1, 0xffffffffb000, 0x4000, (size_t)VIRT_TO_PHYS(t->stack_alloced_ptr));
+    mappages(t->context.ttbr0_el1, 0x0, filesize, (size_t)VIRT_TO_PHYS(t->data));
+
+    t->context.ttbr0_el1 = VIRT_TO_PHYS(t->context.ttbr0_el1);
+    t->context.sp = 0xfffffffff000;
+    t->context.fp = 0xfffffffff000;
+    t->context.lr = 0L;
+
+    t->context.lr = (unsigned long)0L;
+    //copy file into data
+    for (int i = 0; i < filesize;i++)
+    {
+        t->data[i] = data[i];
+    }
+
+    //disable echo when going to userspace
+    uart_disable_echo();
+    curr_thread = t;
+    add_timer(schedule_timer, 1, "", 0);
+    // eret to exception level 0
+    asm("msr tpidr_el1, %0\n\t"
+        "msr elr_el1, %1\n\t"
+        "msr spsr_el1, xzr\n\t" // enable interrupt in EL0. You can do it by setting spsr_el1 to 0 before returning to EL0.
+        "msr sp_el0, %2\n\t"
+        "mov sp, %3\n\t"
+        "msr ttbr0_el1, %4\n\t"
+        "eret\n\t" ::"r"(&t->context),"r"(t->context.lr), "r"(t->context.sp), "r"(t->kernel_stack_alloced_ptr + KSTACK_SIZE), "r"(t->context.ttbr0_el1));
+
+    return 0;
+}
+
+
+//malloc a kstack and a userstack
+thread_t *thread_create(void *start, unsigned int filesize)
+{
+    lock();
+
+    thread_t *r;
+    for (int i = 0; i <= PIDMAX; i++)
+    {
+        if (!threads[i].isused)
+        {
+            r = &threads[i];
+            break;
+        }
+    }
+    r->iszombie = 0;
+    r->isused = 1;
+    r->context.lr = (unsigned long long)start;
+    r->stack_alloced_ptr = kmalloc(USTACK_SIZE);
+    r->kernel_stack_alloced_ptr = kmalloc(KSTACK_SIZE);
+    r->signal_is_checking = 0;
+    r->data = kmalloc(filesize);
+    r->datasize = filesize;
+    r->context.sp = (unsigned long long)r->kernel_stack_alloced_ptr + KSTACK_SIZE;
+    r->context.fp = r->context.sp;
+
+    r->context.ttbr0_el1 = kmalloc(0x1000);
+    memset(r->context.ttbr0_el1, 0, 0x1000);
+
+    //initial signal handler with signal_default_handler (kill thread)
+    for (int i = 0; i < SIGNAL_MAX; i++)
+    {
+        r->singal_handler[i] = signal_default_handler;
+        r->sigcount[i] = 0;
+    }
+
+    list_add(&r->listhead, run_queue);
+    unlock();
+    return r;
+}
+
+void thread_exit(){
+    lock();
+    curr_thread->iszombie = 1;
+    unlock();
+    schedule();
+}
+
+void schedule_timer(char* notuse){
+    unsigned long long cntfrq_el0;
+    __asm__ __volatile__("mrs %0, cntfrq_el0\n\t": "=r"(cntfrq_el0)); //tick frequency
+    add_timer(schedule_timer, cntfrq_el0 >> 5, "", 1);
+}
---
+++ lab6/src/syscall.c
@@ -0,0 +1,188 @@
+#include "syscall.h"
+#include "sched.h"
+#include "stddef.h"
+#include "uart.h"
+#include "filesystem.h"
+#include "exception.h"
+#include "malloc.h"
+#include "mbox.h"
+#include "signal.h"
+#include "mmu.h"
+#include "string.h"
+
+int getpid(trapframe_t* tpf)
+{
+    tpf->x0 = curr_thread->pid;
+    return curr_thread->pid;
+}
+
+size_t uartread(trapframe_t *tpf,char buf[], size_t size)
+{
+    int i = 0;
+    for (int i = 0; i < size;i++)
+    {
+        buf[i] = uart_async_getc();
+    }
+    tpf->x0 = i;
+    return i;
+}
+
+size_t uartwrite(trapframe_t *tpf,const char buf[], size_t size)
+{
+    int i = 0;
+    for (int i = 0; i < size; i++)
+    {
+        uart_async_putc(buf[i]);
+    }
+    tpf->x0 = i;
+    return i;
+}
+
+//In this lab, you won’t have to deal with argument passing
+int exec(trapframe_t *tpf,const char *name, char *const argv[])
+{
+    kfree(curr_thread->data);
+    curr_thread->datasize = get_file_size((char *)name);
+    char *new_data = get_file_start((char *)name);
+    curr_thread->data = kmalloc(curr_thread->datasize);
+
+    //remap code
+    mappages(PHYS_TO_VIRT(curr_thread->context.ttbr0_el1), 0x0, curr_thread->datasize, (size_t)VIRT_TO_PHYS(curr_thread->data));
+
+    for (unsigned int i = 0; i < curr_thread->datasize; i++)
+    {
+        curr_thread->data[i] = new_data[i];
+    }
+
+    //clear signal handler
+    for (int i = 0; i <= SIGNAL_MAX; i++)
+    {
+        curr_thread->singal_handler[i] = signal_default_handler;
+    }
+
+    tpf->elr_el1 = 0;
+    tpf->sp_el0 = 0xfffffffff000;
+    tpf->x0 = 0;
+    return 0;
+}
+
+int fork(trapframe_t *tpf)
+{
+    lock();
+    thread_t *newt = thread_create(curr_thread->data,curr_thread->datasize);
+
+    //copy signal handler
+    for (int i = 0; i <= SIGNAL_MAX;i++)
+    {
+        newt->singal_handler[i] = curr_thread->singal_handler[i];
+    }
+
+    mappages(newt->context.ttbr0_el1, 0x3C000000L, 0x3000000L, 0x3C000000L);
+    mappages(newt->context.ttbr0_el1, 0x3F000000L, 0x1000000L, 0x3F000000L);
+    mappages(newt->context.ttbr0_el1, 0x40000000L, 0x40000000L, 0x40000000L);
+
+    // remap code and stack
+    mappages(newt->context.ttbr0_el1, 0xffffffffb000, 0x4000, (size_t)VIRT_TO_PHYS(newt->stack_alloced_ptr));
+    mappages(newt->context.ttbr0_el1, 0x0, newt->datasize, (size_t)VIRT_TO_PHYS(newt->data));
+
+    //在這段page被蓋爁
+    int parent_pid = curr_thread->pid;
+
+    //copy data into new process
+    for (int i = 0; i < newt->datasize; i++)
+    {
+        newt->data[i] = curr_thread->data[i];
+    }
+
+    //copy user stack into new process
+    for (int i = 0; i < USTACK_SIZE; i++)
+    {
+        newt->stack_alloced_ptr[i] = curr_thread->stack_alloced_ptr[i];
+    }
+
+    //copy stack into new process
+    for (int i = 0; i < KSTACK_SIZE; i++)
+    {
+        newt->kernel_stack_alloced_ptr[i] = curr_thread->kernel_stack_alloced_ptr[i];
+    }
+
+    //在這段page被蓋爁
+    
+    store_context(get_current());
+    //for child
+    if( parent_pid != curr_thread->pid)
+    {
+        goto child;
+    }
+
+    void *temp_ttbr0_el1 = newt->context.ttbr0_el1;
+    newt->context = curr_thread->context;
+    newt->context.ttbr0_el1 = VIRT_TO_PHYS(temp_ttbr0_el1);
+    newt->context.fp += newt->kernel_stack_alloced_ptr - curr_thread->kernel_stack_alloced_ptr; // move fp
+    newt->context.sp += newt->kernel_stack_alloced_ptr - curr_thread->kernel_stack_alloced_ptr; // move kernel sp
+
+    unlock();
+
+    tpf->x0 = newt->pid;
+    return newt->pid;
+
+child:
+    tpf->x0 = 0;
+    return 0;
+}
+
+void exit(trapframe_t *tpf, int status)
+{
+    thread_exit();
+}
+
+int syscall_mbox_call(trapframe_t *tpf, unsigned char ch, unsigned int *mbox_user)
+{
+    //mbox_user = (unsigned int *)(curr_thread->stack_alloced_ptr + USTACK_SIZE - (0xfffffffff000 - (size_t)mbox));
+    lock();
+
+    unsigned int size_of_mbox = mbox_user[0];
+    memcpy((char *)mbox, mbox_user, size_of_mbox);
+    mbox_call(ch);
+    memcpy(mbox_user, (char *)mbox, size_of_mbox);
+
+    tpf->x0 = 8;
+    unlock();
+    return 0;
+}
+
+void kill(trapframe_t *tpf,int pid)
+{
+    lock();
+    if (pid >= PIDMAX || pid < 0  || !threads[pid].isused)
+    {
+        unlock();
+        return;
+    }
+    threads[pid].iszombie = 1;
+    unlock();
+    schedule();
+}
+
+void signal_register(int signal, void (*handler)())
+{
+    if (signal > SIGNAL_MAX || signal < 0)return;
+
+    curr_thread->singal_handler[signal] = handler;
+}
+
+void signal_kill(int pid, int signal)
+{
+    if (pid > PIDMAX || pid < 0 || !threads[pid].isused)return;
+
+    lock();
+    threads[pid].sigcount[signal]++;
+    unlock();
+}
+
+void sigreturn(trapframe_t *tpf)
+{
+    unsigned long signal_ustack = tpf->sp_el0 % USTACK_SIZE == 0 ? tpf->sp_el0 - USTACK_SIZE : tpf->sp_el0 & (~(USTACK_SIZE - 1));
+    kfree((char*)signal_ustack);
+    load_context(&curr_thread->signal_saved_context);
+}
