263007f34007551a71e952b37bc7fc8864cbc8cc
xiaobye <skps96g313@gmail.com>
2022-05-24 04:03:13 +0800

    [lab4]: finish function free_alloc but it may has some bugs and since function free_alloc look too dirty, it should be refactored

--- lab4/include/mm/page_alloc.h
+++ lab4/include/mm/page_alloc.h
@@ -15,6 +15,10 @@
 #define PAGE_SHIFT 12
 #define PAGE_SIZE (1ul << PAGE_SHIFT)
 
+#define BUDDY_IS_ALLOCATED(x) (((struct page*)x)->order & BUDDY_ALLOCATED)
+#define BUDDY_IS_MEMBER(x) (((struct page*)x)->order & BUDDY_GROUP_MEMBER)
+#define BUDDY_IS_FREED(x) (!BUDDY_IS_ALLOCATED(x))
+#define PAGE_IS_RESERVED(x) (((struct page*)x)->type & PAGE_TYPE_RESERVED)
 #define _buddy_ffs(x) ((x) == 0 ? BUDDY_MAX_ORDER : ffs64(x))
 #define addr_to_pfn(addr) ((uint64_t)(addr) >> PAGE_SHIFT)
 #define pfn_to_addr(n) ((void*)(n << PAGE_SHIFT))
@@ -37,7 +41,11 @@ struct free_list{
 struct buddy_system{
     struct free_list free_lists[BUDDY_MAX_ORDER];
 };
+
 extern void buddy_init();
+extern void* alloc_page();
+extern void* alloc_pages(uint32_t);
+
 
 #endif
 
