f820bbda75e756aacb8467d3a7d2630dba0beeca
xiaobye <skps96g313@gmail.com>
2022-05-26 02:26:38 +0800

    [lab4]: Because of some technical problem, I remove rules in Makefile that is used to comple bootloader.If you want to use bootloader, please compile it in lab3 directory

--- lab4/lib/ring_buffer.c
+++ lab4/lib/ring_buffer.c
@@ -1,5 +1,6 @@
 #include "lib/ring_buffer.h"
-ring_buffer* create_ring_buf(size_t s){
+
+ring_buffer* create_simple_ring_buf(size_t s){
     ring_buffer* tmp = (ring_buffer*)simple_malloc(sizeof(ring_buffer));
     tmp->buf = (uint8_t*)simple_malloc(s + 1);
     tmp->size = s;
@@ -7,6 +8,22 @@ ring_buffer* create_ring_buf(size_t s){
     tmp->tail = 0;
     tmp->count = 0;
     return tmp;
+}
+
+// This function can only be used after initializing mm 
+ring_buffer* create_ring_buf(size_t s){
+    ring_buffer* tmp = (ring_buffer*)kmalloc(sizeof(ring_buffer));
+    tmp->buf = (uint8_t*)kmalloc(s + 1);
+    tmp->size = s;
+    tmp->head = 0;
+    tmp->tail = 0;
+    tmp->count = 0;
+    return tmp;
+}
+
+void free_ring_buf(ring_buffer* rbuf){
+    kfree(rbuf->buf);
+    kfree(rbuf);
 }
 
 uint8_t ring_buf_is_empty(ring_buffer* rbuf){
