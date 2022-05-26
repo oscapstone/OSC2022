7491e12b9fd6fabdaf2b08f9f39a9a001e8a5f2c
xiaobye <skps96g313@gmail.com>
2022-05-27 02:16:50 +0800

    demo

--- lab5/peripherals/mini_uart.c
+++ lab5/peripherals/mini_uart.c
@@ -87,7 +87,6 @@ void mini_uart_init(){
 
     // Set Baud rate to 115200
     IO_MMIO_write32(AUX_MU_BAUD_REG, BAUD_RATE_REG);
-
     
     // Start UART
     IO_MMIO_write32(AUX_MU_CNTL_REG, 3);
@@ -138,7 +137,6 @@ void mini_uart_irq_init(){
     IO_MMIO_write32(ENABLE_IRQS_1, 1 << 29);
     // only enable receive interrupt. transmit interrupts should be enable when user want to transmit data.
     enable_mini_uart_irq(RX);
-    enable_mini_uart_irq(RX);
 }
 
 void mini_uart_tx_softirq_callback(){
@@ -148,7 +146,7 @@ void mini_uart_rx_softirq_callback(){
 }
 
 void mini_uart_irq_read(){
-    while((IO_MMIO_read32(AUX_MU_LSR_REG) & 0x1)){
+    if(IO_MMIO_read32(AUX_MU_LSR_REG) & 0x1){
         uint8_t b[1];
         b[0] = IO_MMIO_read32(AUX_MU_IO_REG) & 0xff;
         ring_buf_write(rx_rbuf, b, 1);
