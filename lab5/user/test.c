ff41bcfda967508d4ed12eca3798727a32f5530a
xiaobye <skps96g313@gmail.com>
2022-06-10 07:05:42 +0800

    [lab5]: finish sys_fork

--- lab5/user/test.c
+++ lab5/user/test.c
@@ -6,8 +6,9 @@ int __start(void){
     char buf[15];
     //size_t s;
     uart_write("test fork\r\n", sizeof("test fork\r\n"));
+    uart_write("> \r\n", sizeof(">\r\n"));
     uart_read(buf, 1);
-    uart_write("start fork\r\n", sizeof("test fork\r\n"));
+    uart_write("start fork\r\n", sizeof("start fork\r\n"));
     while(1){
         if(fork() == 0){
             while(1){
