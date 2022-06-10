d443c27d8ae899ffd0a97fcf2d4d98c18d646a00
xiaobye <skps96g313@gmail.com>
2022-06-10 12:13:34 +0800

    [lab5]: test

--- lab5/user/test.c
+++ lab5/user/test.c
@@ -6,19 +6,22 @@ int __start(void){
     int cnt = 1;
     int ret = 0;
     long long cur_sp;
-                asm volatile("mov %0, sp" : "=r"(cur_sp));
+    asm volatile("mov %0, sp" : "=r"(cur_sp));
     printf("Fork test pid: %d, cnt: %d, ptr: %x, sp : %x\r\n", getpid(), cnt, &cnt, cur_sp);
     //debug_info();
     if ((ret = fork()) == 0) { // child
-        //debug_info();
+                               //debug_info();
         asm volatile("mov %0, sp" : "=r"(cur_sp));
         printf("first child pid: %d, cnt: %d, ptr: %x, sp : %x\r\n", getpid(), cnt, &cnt, cur_sp);
         ++cnt;
 
         if ((ret = fork()) != 0){
             //debug_info();
-            asm volatile("mov %0, sp" : "=r"(cur_sp));
-            printf("first child pid: %d, cnt: %d, ptr: %x, sp : %x\r\n", getpid(), cnt, &cnt, cur_sp);
+            while(1){
+                asm volatile("mov %0, sp" : "=r"(cur_sp));
+                printf("first child pid: %d, cnt: %d, ptr: %x, sp : %x\r\n", getpid(), cnt, &cnt, cur_sp);
+                delay(1000000);
+            }
         }
         else{
             //debug_info();
@@ -28,13 +31,21 @@ int __start(void){
                 delay(1000000);
                 ++cnt;
             }
+            while(1){
+                asm volatile("mov %0, sp" : "=r"(cur_sp));
+                printf("second child pid: %d, cnt: %d, ptr: %x, sp : %x\r\n", getpid(), cnt, &cnt, cur_sp);
+                delay(1000000);
+            }
         }
     }
     else {
-            //debug_info();
-                asm volatile("mov %0, sp" : "=r"(cur_sp));
-        printf("parent here, pid: %d, cnt: %d, ptr: %x, sp : %x\r\n", getpid(), cnt, &cnt, cur_sp);
-    } 
+        //debug_info();
+        while(1){
+            asm volatile("mov %0, sp" : "=r"(cur_sp));
+            printf("parent here, pid: %d, cnt: %d, ptr: %x, sp : %x\r\n", getpid(), cnt, &cnt, cur_sp);
+            delay(1000000);
+        }
+    }
     while(1);
 }
 
