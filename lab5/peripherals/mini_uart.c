f0251896333c2e1573ae31053edb4ffabf9917dc
xiaobye <skps96g313@gmail.com>
2022-06-11 08:35:02 +0800

    [lab5]: test

--- lab5/peripherals/mini_uart.c
+++ lab5/peripherals/mini_uart.c
@@ -219,6 +219,7 @@ ssize_t aio_write_bytes(uint8_t* buf, size_t n){
 size_t sys_uart_write(char *buf, size_t size){
     uint64_t daif;
     size_t c = 0, tmp;
+    size_t count = 0;
     while(size){
         daif = local_irq_disable_save();
         tmp = ring_buf_write(tx_rbuf, buf + c, size);
@@ -229,6 +230,11 @@ size_t sys_uart_write(char *buf, size_t size){
             write_str("sys_uart_write error\r\n");
             while(1);
         }
+        write_hex(count);
+        write_str("\r\n");
+        count++; 
+
+
         size = size - tmp;
         c = c + tmp;
     }
